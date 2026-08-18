7d92e98f0a2d1e61f3d2a258b2d06425ae22672c
SERGIORAMOS010599 <sergior@idttecnologias.mx>
2026-08-18 11:53:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app) # <-- ¡ESTE ES EL PASE VIP PARA GITHUB!
 
 URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbwQtH2kJfqcZePWE7mbj-Vq17voBp-wL0xoMfvhHCxriw_WfHnIXVnDUgUrHrOFfi8/exec"
 
 def obtener_datos(nombre_hoja):
     try:
-        # Le pegamos el parámetro '?hoja=' al final de la URL
         respuesta = requests.get(f"{URL_APPS_SCRIPT}?hoja={nombre_hoja}")
         if respuesta.status_code == 200:
             return jsonify(respuesta.json())
@@ -19,17 +20,14 @@ def obtener_datos(nombre_hoja):
 def inicio():
     return "¡Servidor PU-K en línea! Rutas: /api/salidas, /api/lineas, /api/sensores"
 
-# Tubería 1: Salidas
 @app.route('/api/salidas')
 def salidas():
     return obtener_datos("salidas")
 
-# Tubería 2: Líneas Nuevas
 @app.route('/api/lineas')
 def lineas():
     return obtener_datos("lineas nuevas")
 
-# Tubería 3: Sensores
 @app.route('/api/sensores')
 def sensores():
     return obtener_datos("sensroesFyTm")
